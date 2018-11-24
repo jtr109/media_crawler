bd3f16efb3e40b45b7270400b1afc36406c9e72d
jtr109 <lyp_login@outlook.com>
2018-11-24 12:21:38 +0800

    feat(mc): handle post method

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ async def handle(request):
     text = "Hello World!"
     return web.Response(text=text)
 
-app = web.Application()
-app.add_routes([
-    web.get('/', handle),
-    web.get('/parse_media', handle),
-])
 
-web.run_app(app)
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle),
+        web.post('/parse_media', handle),
+    ])
+
+    web.run_app(app)
